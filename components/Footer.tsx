6418a99603689c38491d20ee522749e431c3deef
Ramanath K P <ramnathnaik447@gmail.com>
2025-05-25 21:06:39 +0530

    footer width fix

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -2,7 +2,7 @@
 
 export default function Footer() {
   return (
-    <footer className="bg-[#FFC637] text-black py-6 mt-12 shadow-inner">
+    <footer className="bg-[#FFC637] text-black py-6 mt-12 shadow-inner w-full">
       <div className="container mx-auto px-6 text-center text-xs font-semibold">
         ನನ್ನ ಜಗತ್ತಿಗೆ ಭೇಟಿ ನೀಡಿದ್ದಕ್ಕೆ ಧನ್ಯವಾದಗಳು! ಮತ್ತೆ ಸಿಗೋಣ! 😊❤️
       </div>
